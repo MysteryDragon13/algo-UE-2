5a089508cbcbae0ddbbf1137a45e225dcdbd6b6d
s2310238020 <s2310238020@fhooe.at>
2024-12-09 17:12:22 +0100

    ue4 done

---
+++ src/UE2/MyBTree.java
@@ -0,0 +1,42 @@
+package UE2;
+
+import kapitel_3.Queue;
+import kapitel_3.vl.BTree;
+
+public class MyBTree extends BTree{
+
+    protected void breadthFirstAppend(Node newNode){
+        //copied and edited breadthFirst
+        Queue queue = new Queue();			       // The helper-queue
+
+        if (root != null) {
+            queue.enqueue(root);				   // Enqueue the root of the tree
+        }else{
+            root = newNode;
+            return;
+        }
+        while (!queue.empty()) {                   // Iterate as long as queue is not empty
+            Node current = (Node) queue.dequeue(); // Fetch a node from the queue
+            if (current.left != null) {            // If the left child of this node exists
+                queue.enqueue(current.left);       // enqueue this child
+            }else{
+                current.left = newNode;
+                return;
+            }
+            if (current.right != null) {           // If the right child of the node exists
+                queue.enqueue(current.right);      // enqueue it also
+            }else{
+                current.left = newNode;
+                return;
+            }
+        }
+
+    }
+    public void insert(Object data) {
+        //copied and edited breadthFirstAppend
+        Queue queue = new Queue();                   // The helper-queue
+        Node newNode = new Node(null, data, null);
+        breadthFirstAppend(newNode);
+    }
+}
+
---
+++ src/UE2/Stack.java
@@ -0,0 +1,20 @@
+package UE2;
+
+import kapitel_3.vl.SList;
+
+public class Stack extends SList{
+    public void push(Object data){
+        head = new Node(data, head.next);
+    }
+    public Object pop(){
+        Node temp = head;
+        head=head.next;
+        return temp.data;
+    }
+    public Object peek(){
+        return head.data;
+    }
+    public boolean empty(){
+        return head == null;
+    }
+}
